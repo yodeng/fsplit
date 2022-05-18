154e09bb7f1029b3382d9ea3c6bef4a00e28f16d
yodeng <yodeng@tju.edu.cn>
2022-05-18 20:44:02 +0800

    add random index position and remove manager out queue

---
+++ src/bcl.py
@@ -0,0 +1,93 @@
+import os
+import sh
+import sys
+import json
+import logging
+import subprocess
+
+
+class BCL(object):
+    def __init__(self, fcdir, outdir, bcfile, cpu=60, bcl2fastq="", mis=1):
+        self.fcdir = fcdir
+        self.outdir = outdir
+        self.bcfile = bcfile
+        self.mis = str(mis)
+        self.index = 1
+        self.nproc = str(cpu)
+        self.bcl2fastq = bcl2fastq or sh.which("bcl2fastq")
+        self.samplesheet = os.path.join(self.outdir, "sample-sheet.csv")
+
+    def create_samplesheet(self):
+        idx = []
+        with open(self.bcfile) as fi:
+            for line in fi:
+                if not line.strip() or line.startswith("#"):
+                    continue
+                line = line.split()
+                if len(line) == 2:
+                    self.index = 1
+                    idx.append((line))
+                elif len(line) == 3:
+                    self.index = 2
+                    idx.append((line))
+                else:
+                    raise IOError("illegal barcode file %s" % self.bcfile)
+        if not os.path.isdir(self.outdir):
+            os.makedirs(self.outdir)
+        with open(self.samplesheet, "w") as fo:
+            if self.index == 1:
+                fo.write("[Data]\nSample_ID,Sample_Name,index\n")
+            else:
+                fo.write("[Data]\nSample_ID,Sample_Name,index,index2\n")
+            for line in idx:
+                fo.write(",".join([line[0]] + line) + "\n")
+
+    def make_bcl_cmd(self):
+        cmd = [self.bcl2fastq, "-o", self.outdir, "-R", self.fcdir,
+               "-i", os.path.join(self.fcdir, "Data/Intensities/BaseCalls"),
+               "--no-lane-splitting",
+               "--barcode-mismatches", self.mis,
+               "-p", self.nproc,
+               "--sample-sheet", self.samplesheet]
+        return " ".join(cmd)
+
+    @staticmethod
+    def call(cmd, run=True, verbose=False):
+        if not run:
+            if verbose:
+                print(cmd)
+            return
+        if verbose:
+            print(cmd)
+            subprocess.check_call(cmd, shell=True, stdout=sys.stdout,
+                                  stderr=sys.stderr)
+        else:
+            with open(os.devnull, "w") as fo:
+                subprocess.check_call(cmd, shell=True, stdout=fo, stderr=fo)
+
+    def stats(self, j):
+        with open(j) as fi:
+            stat = json.load(fi)
+        info = stat["ConversionResults"][0]
+        out = []
+        for i in info["DemuxResults"]:
+            sn = i["SampleName"]
+            rdn = i["NumberReads"]
+            out.append((sn, rdn))
+        out.append(("Unknow", info["Undetermined"]["NumberReads"]))
+        return out
+
+    def run(self):
+        self.create_samplesheet()
+        cmd = self.make_bcl_cmd()
+        self.cmd = cmd
+        # self.logs.info(cmd)
+        self.logs.info("start bcl2fastq.")
+        self.call(cmd, run=True)
+        #sms = self.stats(os.path.join(self.outdir, "Stats/Stats.json"))
+        # self.logs.info(sms)
+        # self.logs.info("Success")
+
+    @property
+    def logs(self):
+        return logging.getLogger()
--- src/main.py
+++ src/main.py
@@ -15,14 +15,35 @@ from .src import *
 def main():
     logs = log()
     args = parseArg()
-    if not os.path.exists(args.input):
-        IOError("No such file or directory: %s" % args.input)
-    if args.mode == "index":
-        logs.info("start fsplit")
-        FastqIndex.createindex(args.input)
-        return
     infq = args.input
     outdir = os.path.abspath(args.output)
+    if not os.path.exists(infq):
+        IOError("No such file or directory: %s" % infq)
+    logs.info("start fsplit")
+    if args.mode == "index":
+        FastqIndex.createindex(args.input)
+        return
+    if os.path.isdir(infq):
+        if os.path.isfile(os.path.join(infq, "RTAComplete.txt")):
+            bcl2fastq = args.bcl2fq or sh.which("bcl2fastq") or os.path.join(
+                sys.prefix, "bin", "bcl2fastq")
+            if not os.path.isfile(bcl2fastq):
+                sys.exit("bcl2fastq not found, exists")
+            bcl = BCL(infq, outdir, args.barcode, args.threads,
+                      bcl2fastq=bcl2fastq, mis=args.mismatch)
+            bcl.run()
+            logs.info("Success")
+            js = os.path.join(outdir, "Stats/Stats.json")
+            if os.path.isfile(js):
+                stats = load_bcl_stats(js)
+                logs.info(stats)
+            rmpdir = ["Stats", "Reports", "sample-sheet.csv"]
+            rmfile = [os.path.join(outdir, i) for i in rmpdir]
+            rmcmd = "rm -fr %s" % " ".join(rmfile)
+            call(rmcmd)
+            return
+        else:
+            sys.exit("flowcell directory '%s' incorrection" % infq)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     group = args.threads * 5
@@ -38,70 +59,36 @@ def main():
             sn, bc = line[0], line[1]
             barcode[bc.encode()] = sn
             outfile[sn] = os.path.join(outdir, sn+".fq")
-    if args.output_gzip:
-        for sn in outfile:
-            outfile[sn] += ".gz"
-    logs.info("start fsplit")
-    if os.path.isdir(infq):
-        if os.path.isfile(os.path.join(infq, "RTAComplete.txt")):
-            with open(os.path.join(outdir, "sample-sheet.csv"), "w") as fo:
-                fo.write("[Data]\nSample_ID,Sample_Name,index\n")
-                for bcs, bcn in barcode.items():
-                    bcs = bcs.decode()
-                    fo.write(",".join([bcn, bcn, bcs]) + "\n")
-            bcl2fastq = args.bcl2fq or sh.which("bcl2fastq") or os.path.join(
-                sys.prefix, "bin", "bcl2fastq")
-            if not os.path.isfile(bcl2fastq):
-                sys.exit("bcl2fastq not found, exists")
-            cmd = [bcl2fastq, "-o", outdir, "-R", infq, "--no-lane-splitting"]
-            cmd.extend(["--barcode-mismatches", str(args.mismatch)])
-            cmd.extend(["-p", str(args.threads)])
-            cmd.extend(
-                ["--sample-sheet", os.path.join(outdir, "sample-sheet.csv")])
-            cmd.extend(
-                ["-i", os.path.join(infq, "Data/Intensities/BaseCalls")])
-            logs.info(" ".join(cmd))
-            call(" ".join(cmd))
-            logs.info("Success")
-            sys.stdout.write("\n")
-            js = os.path.join(outdir, "Stats/Stats.json")
-            if os.path.isfile(js):
-                stats = load_bcl_stats(js)
-                for sn, rdn in stats:
-                    sys.stdout.write("%s: %d\n" % (sn, rdn))
-            rmpdir = ["Stats", "Reports", ]
-            rmfile = [os.path.join(outdir, i) for i in rmpdir]
-            rmcmd = "rm -fr %s" % " ".join(rmfile)
-            call(rmcmd)
-            return
     idx = FastqIndex.getIndexPos(infq)
     size = int(math.ceil(len(idx)/float(group)))
     l = len(idx)
     pos = []
     for i in range(0, len(idx), size):
-        e = min(l, i+size)
-        pos.append((idx[i], idx[e-1]))
+        e = min(l-1, i+size)
+        pos.append((idx[i], idx[e]))
     outQ = mp.Manager().Queue()
     p = mp.Pool(args.threads)
     for s, e in pos:
         p.apply_async(splitFastq,  args=(infq, int(s), int(e),
-                      outQ, barcode, args.mismatch, args.drup))
+                      outQ, barcode, args.mismatch, args.drup, outdir))
 
     sms = Counter()
     ivs = len(pos)
     count = 0
-    with MultiZipHandle(**outfile) as fh:
-        while count < ivs:
-            res_p = outQ.get()
-            for sn, seqs in res_p.items():
-                h = fh[sn]
-                sms[sn] += len(seqs) / 4
-                for seq in seqs:
-                    h.write(seq)
-            count += 1
+    total_seq = 0
+    for _, f in outfile.items():
+        if os.path.isfile(f):
+            call("rm -fr " + f)
+    for s, e in pos:
+        res = outQ.get()
+        total, snms, snoutf = res
+        sms.update(snms)
+        total_seq += total
+        for sn, _of in snoutf.items():
+            call("cat %s >> %s" % (_of, outfile[sn]))
+            call("rm -fr " + _of)
     logs.info("Success")
     sys.stdout.write("\n")
-    total_seq = l
     sms["Unknow"] = total_seq - sum(sms.values())
     for sn in sorted(barcode.values()):
         num = sms[sn]
