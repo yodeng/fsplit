84491a103b351cd97e4c6175dccb48b5b6dac9f9
dengyong <dengyong@hugobiotech.com>
2022-05-18 16:22:57 +0800

    add bcl flowcell split by bcl2fastq

--- src/main.py
+++ src/main.py
@@ -13,14 +13,16 @@ from .src import *
 
 @timeRecord
 def main():
+    logs = log()
     args = parseArg()
-    if not os.path.isfile(args.input):
-        sys.exit("No such file %s" % args.input)
+    if not os.path.exists(args.input):
+        IOError("No such file or directory: %s" % args.input)
     if args.mode == "index":
+        logs.info("start fsplit")
         FastqIndex.createindex(args.input)
         return
     infq = args.input
-    outdir = args.output
+    outdir = os.path.abspath(args.output)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     group = args.threads * 5
@@ -39,6 +41,39 @@ def main():
     if args.output_gzip:
         for sn in outfile:
             outfile[sn] += ".gz"
+    logs.info("start fsplit")
+    if os.path.isdir(infq):
+        if os.path.isfile(os.path.join(infq, "RTAComplete.txt")):
+            with open(os.path.join(outdir, "sample-sheet.csv"), "w") as fo:
+                fo.write("[Data]\nSample_ID,Sample_Name,index\n")
+                for bcs, bcn in barcode.items():
+                    bcs = bcs.decode()
+                    fo.write(",".join([bcn, bcn, bcs]) + "\n")
+            bcl2fastq = args.bcl2fq or sh.which("bcl2fastq") or os.path.join(
+                sys.prefix, "bin", "bcl2fastq")
+            if not os.path.isfile(bcl2fastq):
+                sys.exit("bcl2fastq not found, exists")
+            cmd = [bcl2fastq, "-o", outdir, "-R", infq, "--no-lane-splitting"]
+            cmd.extend(["--barcode-mismatches", str(args.mismatch)])
+            cmd.extend(["-p", str(args.threads)])
+            cmd.extend(
+                ["--sample-sheet", os.path.join(outdir, "sample-sheet.csv")])
+            cmd.extend(
+                ["-i", os.path.join(infq, "Data/Intensities/BaseCalls")])
+            logs.info(" ".join(cmd))
+            call(" ".join(cmd))
+            logs.info("Success")
+            sys.stdout.write("\n")
+            js = os.path.join(outdir, "Stats/Stats.json")
+            if os.path.isfile(js):
+                stats = load_bcl_stats(js)
+                for sn, rdn in stats:
+                    sys.stdout.write("%s: %d\n" % (sn, rdn))
+            rmpdir = ["Stats", "Reports", ]
+            rmfile = [os.path.join(outdir, i) for i in rmpdir]
+            rmcmd = "rm -fr %s" % " ".join(rmfile)
+            call(rmcmd)
+            return
     idx = FastqIndex.getIndexPos(infq)
     size = int(math.ceil(len(idx)/float(group)))
     l = len(idx)
@@ -64,6 +99,8 @@ def main():
                 for seq in seqs:
                     h.write(seq)
             count += 1
+    logs.info("Success")
+    sys.stdout.write("\n")
     total_seq = l
     sms["Unknow"] = total_seq - sum(sms.values())
     for sn in sorted(barcode.values()):
