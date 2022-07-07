22e1ddd80b9a0ddd3dc3ef5c4d8da36d36d2d51d
yodeng <yodeng@tju.edu.cn>
2022-07-07 21:59:03 +0800

    add gsplit read2 support for multi barcode sequence per sample [version paired end, can be used for umi split v2022.07.08 15:00]

--- src/gsplit.go
+++ src/gsplit.go
@@ -13,10 +13,11 @@ import (
 	"github.com/yodeng/xopen"
 )
 
-const VERSION = "v2022.07.07 15:00"
+const VERSION = "v2022.07.08 15:00"
 
 type SplitFlags struct {
 	Fqfile      []string `hflag:"--input, -i; required; usage: input fastq file, *.gz/xz/zst or uncompress allowed, multi-input can be separated by ',' or whitespace, required"`
+	Fqfile2     []string `hflag:"--Input, -I; usage: input read2 fastq file if there is, *.gz/xz/zst or uncompress allowed, multi-input can be separated by ',' or whitespace"`
 	Barcodefile string   `hflag:"--barcode, -b; required; usage: barcode and sample file, 1st column for sample name and 2nd column for barcode sequence, required"`
 	Output      string   `hflag:"--output, -o; required; usage: output directory, will create if not exists, required"`
 	Threads     int      `hflag:"--threads, -t; default: 10; usage: threads core, 10 by default"`
@@ -31,6 +32,41 @@ func checkError(err error) {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func splitSample(seq string, bc map[string]string, mis int) (name, barcode string) {
+BCLOOP:
+	for b, sn := range bc {
+		d := 0
+		for n := range b {
+			if seq[n] != b[n] {
+				d++
+			}
+			if d > mis {
+				continue BCLOOP
+			}
+		}
+		return sn, b
+	}
+	return
+}
+
+func splitBarcode(seq string, bc []string, mis int) (barcode string) {
+BCLOOP:
+	for _, b := range bc {
+		d := 0
+		for n := range b {
+			if seq[n] != b[n] {
+				d++
+			}
+			if d > mis {
+				continue BCLOOP
+			}
+		}
+		barcode = b
+		break BCLOOP
+	}
+	return
 }
 
 func ReadSeq(ch1 chan []string, infile string) {
@@ -217,7 +253,7 @@ func main() {
 	checkError(err)
 	defer bcf.Close()
 
-	fout := make(map[string]*xopen.Writer, 10)
+	fout := make(map[string][]*xopen.Writer, 10)
 
 	if !isExist(args.Output) {
 		err := os.MkdirAll(args.Output, os.ModePerm)
@@ -225,6 +261,12 @@ func main() {
 	}
 	drup_pos := make(map[string]int, 10)
 	samples := []string{}
+	if args.Fqfile2 != nil {
+		if len(args.Fqfile2) != len(args.Fqfile) {
+			fmt.Println("Miss input fastq file of R1 or R2")
+			os.Exit(1)
+		}
+	}
 	for {
 		line, err := bcf.ReadString('\n')
 		if err == io.EOF {
@@ -238,77 +280,119 @@ func main() {
 		line_s := reg.Split(line, -1)
 		sn, bc := line_s[0], line_s[1]
 		barcode[bc] = sn
+		if len(line_s) >= 3 {
+			barcode[line_s[2]] = sn
+		}
 		if args.Drup {
 			drup_pos[bc] = len(bc)
 		}
 		if !inSlice(samples, sn) {
 			samples = append(samples, sn)
-			outf := args.Output + "/" + sn + ".fq"
-			if !args.Nogz {
-				outf += ".gz"
+			if args.Fqfile2 != nil {
+				outf1 := args.Output + "/" + sn + ".R1.fq"
+				outf2 := args.Output + "/" + sn + ".R2.fq"
+				if !args.Nogz {
+					outf1 += ".gz"
+					outf2 += ".gz"
+				}
+				fo1, _ := xopen.Wopen(outf1)
+				defer fo1.Close()
+				fo2, _ := xopen.Wopen(outf2)
+				defer fo2.Close()
+				fout[sn] = append(fout[sn], []*xopen.Writer{fo1, fo2}...)
+			} else {
+				outf := args.Output + "/" + sn + ".fq"
+				if !args.Nogz {
+					outf += ".gz"
+				}
+				fo, _ := xopen.Wopen(outf)
+				defer fo.Close()
+				fout[sn] = append(fout[sn], fo)
 			}
-			fo, _ := xopen.Wopen(outf)
-			defer fo.Close()
-			fout[line_s[0]] = fo
+
 		}
 	}
-	/*
-		unkonwfile := args.Output + "/" + "Unknow" + ".fq"
-		if !args.Nogz {
-			unkonwfile += ".gz"
-		}
-		unfo, _ := xopen.Wopen(unkonwfile)
-		defer unfo.Close()
-		fout["Unknow"] = unfo
-	*/
 	mis := args.Mismatch
 	seq := [4]string{}
 	sms := make(map[string]int, len(barcode))
+	sample2barcode := make(map[string][]string, len(samples))
+	for b, sn := range barcode {
+		sample2barcode[sn] = append(sample2barcode[sn], b)
+	}
 	total := 0
-	for _, fqfile := range args.Fqfile {
+	for fn, fqfile := range args.Fqfile {
 		r, err := xopen.Ropen(fqfile)
 		checkError(err)
 		defer r.Close()
 		linefo := 0
-		for {
-			line, err := r.ReadString('\n')
-			if err == io.EOF {
-				break
-			}
-			i := linefo % 4
-			seq[i] = line
-			// unknow := true
-			if i == 3 {
-			BCLOOP:
-				for b, sn := range barcode {
-					d := 0
-					for n := range b {
-						if seq[1][n] != b[n] {
-							d++
-						}
-						if d > mis {
-							continue BCLOOP
-						}
-					}
-					// unknow = false
-					seq[1] = seq[1][drup_pos[b]:]
-					seq[3] = seq[3][drup_pos[b]:]
-					sms[sn] += 1
-					for _, line := range seq {
-						fout[sn].WriteString(line)
-					}
-					break BCLOOP
+		if args.Fqfile2 != nil {
+			r2, err := xopen.Ropen(args.Fqfile2[fn])
+			checkError(err)
+			defer r2.Close()
+			seq2 := [4]string{}
+			for {
+				line, err := r.ReadString('\n')
+				if err == io.EOF {
+					break
 				}
-				/*
-				   if unknow {
-				       for _, line := range seq {
-				           fout["Unknow"].WriteString(line)
-				       }
-				   }
-				*/
-				total += 1
+				line2, _ := r2.ReadString('\n')
+				i := linefo % 4
+				seq[i] = line
+				seq2[i] = line2
+				if i == 3 {
+					sn, b1 := splitSample(seq[1], barcode, mis)
+					if sn != "" {
+						b2 := splitBarcode(seq2[1], sample2barcode[sn], mis)
+						if b2 != "" {
+							seq[1] = seq[1][drup_pos[b1]:]
+							seq[3] = seq[3][drup_pos[b1]:]
+							seq2[1] = seq2[1][drup_pos[b2]:]
+							seq2[3] = seq2[3][drup_pos[b2]:]
+							sms[sn] += 1
+							for _, line := range seq {
+								fout[sn][0].WriteString(line)
+							}
+							for _, line := range seq2 {
+								fout[sn][1].WriteString(line)
+							}
+						}
+					}
+					total += 1
+				}
+				linefo++
 			}
-			linefo++
+		} else {
+			for {
+				line, err := r.ReadString('\n')
+				if err == io.EOF {
+					break
+				}
+				i := linefo % 4
+				seq[i] = line
+				if i == 3 {
+				BCLOOP:
+					for b, sn := range barcode {
+						d := 0
+						for n := range b {
+							if seq[1][n] != b[n] {
+								d++
+							}
+							if d > mis {
+								continue BCLOOP
+							}
+						}
+						seq[1] = seq[1][drup_pos[b]:]
+						seq[3] = seq[3][drup_pos[b]:]
+						sms[sn] += 1
+						for _, line := range seq {
+							fout[sn][0].WriteString(line)
+						}
+						break BCLOOP
+					}
+					total += 1
+				}
+				linefo++
+			}
 		}
 	}
 	fmt.Println()
